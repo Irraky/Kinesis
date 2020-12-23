40eb3cad4480289ee6bb047fcafa80ab49d6ccce
irraky <drecours@student.42.fr>
2020-12-23 16:35:20 +0100

    add pin control from raspi
    when packet received, send 1 through pin 17

--- kinesisServerRaspi.py
+++ kinesisServerRaspi.py
@@ -1,13 +1,23 @@
 import socket
 import sys
+import TPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+PINARDUINO = 17
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the port
-server_address = ('ip', 7000)
+server_address = ('192.168.0.159', 7000)
 print('starting up on %s port %s' % server_address)
 sock.bind(server_address)
+
+
+# setup pin 17 as output 
+GPIO.setup(PINARDUINO, GPIO.OUT)
+GPIO.output(PINARDUINO, 0)
 
 # Listen for incoming connections
 sock.listen(1)
@@ -18,6 +28,7 @@ while True:
     connection, client_address = sock.accept()
     try:
         print('connection from', client_address)
+        GPIO.output(PINARDUINO, 1)
         # Receive the data in small chunks and retransmit it
         while True:
             data = connection.recv(16)
@@ -28,6 +39,8 @@ while True:
             else:
                 print('no more data from', client_address)
                 break
+        time.sleep(3)
+        GPIO.output(PINARDUINO, 0)
                 
     finally:
         # Clean up the connection
